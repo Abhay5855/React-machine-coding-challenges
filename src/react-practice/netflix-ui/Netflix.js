499cd0500634fa3949d7d158f11b0c4678578c4e
Abhay <abhay.patil@i2itherapy.com>
2023-03-02 20:01:32 +0530

    feat:netflix ui cards display using props in react js

--- src/react-practice/netflix-ui/Netflix.js
+++ src/react-practice/netflix-ui/Netflix.js
@@ -1,49 +1,25 @@
-
-
-import React from 'react'
-import { movieList } from '../../data/data'
+import React from "react";
+import { movieList } from "../../data/data";
 
 const Netflix = () => {
   return (
     <div>
-     
-     {
-        movieList.map((item) => (
-                   <div key={item.id}>
-
-               
-                  <Card img={item.img} title={item.title} id={item.id}/>
-
-                  </div>
-               
-             
-        ))
-     }
-
-      
+      {movieList.map((item) => (
+        <div key={item.id}>
+          <Card img={item.img} title={item.title} id={item.id} />
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-const Card = ({img , title}) => {
+const Card = ({ img, title }) => {
+  return (
+    <div>
+      <img className="netflix-img" src={img} alt="img" />
+      <p>{title}</p>
+    </div>
+  );
+};
 
-       return (
-
-            <div>
-
-                <img className='netflix-img' src={img} alt='img'/>
-                <p>{title}</p>
-             
-            </div>
-       )
-
-   
-
-}
-
-
-
-
-
-export default Netflix
-
+export default Netflix;
